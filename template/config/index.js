798329bfcb8c6086ae8ea641ef41b713fca8642c
Unknown <tmcnamara@phytest.com>
2017-11-06 09:04:57 -0500

    add webpack proxy config

--- template/config/index.js
+++ template/config/index.js
@@ -30,7 +30,13 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'https://orders.avantlaboratories.com/',
+        changeOrigin: true,
+        cookieDomainRewrite: 'localhost'
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
