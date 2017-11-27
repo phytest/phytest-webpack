aee5945dc6c15633baedc25d0c02aea6114163c5
Unknown <tmcnamara@phytest.com>
2017-11-27 13:38:43 -0500

    Remove reference to logo.png

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,12 +1,9 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    {{#router}}
-    <router-view/>
-    {{else}}
-    <HelloWorld/>
-    {{/router}}
-  </div>
+    <div id="app">
+        {{#router}}
+        <router-view/> {{else}}
+        <HelloWorld/> {{/router}}
+    </div>
 </template>
 
 <script>
